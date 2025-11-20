3fe5dd7650ba8ba0732fa65e88770f0259ee0510
morningbeanss <clrbear150@gmail.com>
2025-11-20 14:05:01 -0800

    made all the tilesets

---
+++ assets/classSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="classSet" tilewidth="16" tileheight="16" spacing="1" tilecount="6893" columns="113">
+ <image source="class_tilesheet.png" width="1936" height="1052"/>
+</tileset>
