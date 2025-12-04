1ac44d5c3c554e307009adaa44b5b05a89f74e36
morningbeanss <clrbear150@gmail.com>
2025-12-04 09:56:15 -0800

    apartment tilemap donezo i think

--- assets/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="grass" tilewidth="16" tileheight="16" tilecount="378" columns="21">
- <image source="interior2/Tiled_files/ground_grass_details.png" width="336" height="288"/>
-</tileset>
